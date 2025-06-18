fb0d604b04897ad42172d3a06f23949554cf588d
TatyanaKravtsova <94558151+TatyanaKravtsova@users.noreply.github.com>
2025-06-18 15:54:31 +0300

    Update index.js

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,10 +2,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const menuButton = document.querySelector('.main-header__menu-button');
   const nav = document.querySelector('.main-header__nav');
-
-  // Удаляем класс --nojs если JS работает
-  nav.classList.remove('main-header__nav--nojs');
-
+  const header = document.querySelector('.main-header');
+  nav.classList.remove('no-js');
+  menuButton.classList.remove('no-js');
+  header.classList.remove('no-js');
   menuButton.addEventListener('click', () => {
     nav.classList.toggle('main-header__nav--opened');
     menuButton.classList.toggle('main-header__menu-button--opened');
