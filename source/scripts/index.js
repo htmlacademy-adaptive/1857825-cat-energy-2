a232ca7c8358db0201de878d903930f92783dd74
TatyanaKravtsova <94558151+TatyanaKravtsova@users.noreply.github.com>
2025-06-18 15:46:31 +0300

    Добавление контентной графики (#6)
    * Добавление контентной графики
    
    * исправление BEM
    
    * fix  html bags

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,13 @@
 /* в этот файл добавляет скрипты*/
+document.addEventListener('DOMContentLoaded', () => {
+  const menuButton = document.querySelector('.main-header__menu-button');
+  const nav = document.querySelector('.main-header__nav');
+
+  // Удаляем класс --nojs если JS работает
+  nav.classList.remove('main-header__nav--nojs');
+
+  menuButton.addEventListener('click', () => {
+    nav.classList.toggle('main-header__nav--opened');
+    menuButton.classList.toggle('main-header__menu-button--opened');
+  });
+});
